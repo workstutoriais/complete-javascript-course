6d92074d4985bcf870ccbee3d4c6fb457457c895
CARLOS G T ALMEIDA <105361453+workstutoriais@users.noreply.github.com>
2023-10-12 10:45:35 +0200

    Added all the projects and c
    Java Script course + Guess hte number changes

---
+++ complete-javascript-course-master/05-Guess-My-Number/starter/script.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const x = function (){
+
+    console.log(23);
+};
+
+
+document.querySelector('.check').addEventListener('click', function) {
+    const guess = Number(document.querySelector('.guess').value);
+
+    console.log(guess, typeof guess);
+
+    
+} 
