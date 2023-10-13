7a1c8882e6a2e372d89c592fe25796f42ae670a9
CARLOS G T ALMEIDA <105361453+workstutoriais@users.noreply.github.com>
2023-10-14 00:16:00 +0200

    Guess Number and Pig Game
    Module 6 and Module 7

---
+++ complete-javascript-course-master/05-Guess-My-Number/starter/scriptgn.js
@@ -0,0 +1,91 @@
+'use strict';
+/*
+console.log(document.querySelector('.message').textContent);
+
+document.querySelector('.message').textContent = '🎉 Correct Number!';
+document.querySelector('.number').textContent = 13;
+
+
+
+
+document.querySelector('.check').addEventListener('click', function) {
+    const guess = Number(document.querySelector('.guess').value);
+
+    console.log(guess, typeof guess);
+
+    
+}  */
+
+const x = function (){
+
+    console.log(23);
+};
+
+let score = 20;
+let highScore = 0;
+
+
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+document.querySelector('.number').textContent = secretNumber;
+
+const displayMessage = function(message) {
+    document.querySelector('.message').textContent = message;
+}
+
+document.querySelector('.check').addEventListener
+('click', function(){
+    const guess = Number(document.querySelector('.guess').value);
+   
+    console.log(guess, typeof guess);
+
+     // When there is no input
+    if(!guess) {
+
+        displayMessage('No Number!');
+    // When the player wins
+    }else if(guess === secretNumber){
+
+      
+        displayMessage( '🎉 Correct Number!');
+        document.querySelector('.number').textContent =secretNumber;
+        document.querySelector('body').style.background = '#60b347';
+        document.querySelector('.number').style.width='30rem';
+
+        if(score > highScore){
+            highScore = score;
+            document.querySelector('.highscore').textContent = highScore;
+        }
+
+     // When guess is wrong
+    } else if(guess !== secretNumber){
+        if(score > 1){
+        
+            displayMessage( guess > secretNumber ? 'Too High!' : 'Too Low!!');
+            score --;
+            document.querySelector('.score').textContent = score;
+     
+    }else {
+            document.querySelector('.message').textContent = 'You Lost the Game';
+            document.querySelector('.score').textContent = 0;
+        }
+    
+         // When the guess is too low
+    } 
+
+});
+
+
+document.querySelector('.again').addEventListener
+('click', function(){
+    score = 20;
+    secretNumber = Math.trunc(Math.random() * 20)+1;
+
+    document.querySelector('.message').textContent = 'Start Guessing...';
+    document.querySelector('.score').textContent =score;
+    document.querySelector('.number').textContent = '?';
+    document.querySelector('.guess').value = '';
+
+    document.querySelector('body').style.backgroundColor = '#222';
+    document.querySelector('.number').style.width = '15rem';
+
+});
--- complete-javascript-course-master/07-Pig-Game/starter/script.js
+++ complete-javascript-course-master/07-Pig-Game/starter/script.js
@@ -1,1 +1,55 @@
 'use strict';
+
+
+//selecting the elementes
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
+const score0El = document.querySelector('#score--0');
+const score1El = document.getElementById('score--1');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
+
+const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+
+
+
+//Starting conditions
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add('hidden');
+
+const scores = [0,0];
+let currentScore = 0;
+let activePlayer = 0;
+
+
+//Rolling Functionality
+btnRoll.addEventListener('click', function(){
+    //1. Generating dice functionality
+    const dice = Math.trunc(Math.random() * 6) +1;
+    console.log(dice);
+
+    //Display Dice
+    diceEl.classList.remove('hidden');
+    diceEl.src = `dice-${dice}.png`;
+
+    //Check For rolled 1
+    if(dice !==1){
+        //add dice to current score
+        currentScore +=dice;
+        document.getElementById(`current--${activePlayer}`).textContent=currentScore;
+        
+    }else {
+        // Sweitch to the next player
+        document.getElementById(`current--${activePlayer}`).textContent=0;
+        activePlayer = activePlayer === 0 ? 1 : 0;
+        currentScore = 0;
+        player0El.classList.toggle('player--active');
+        player1El.classList.toggle('player--active');
+    }
+
+
+});
