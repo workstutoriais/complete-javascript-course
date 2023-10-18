208246d6e323b134c81700de3324fce018c369f9
CARLOS G T ALMEIDA <105361453+workstutoriais@users.noreply.github.com>
2023-10-18 23:43:16 +0200

    10-Functions
    10-Functions

--- complete-javascript-course-master/10-Functions/starter/script.js
+++ complete-javascript-course-master/10-Functions/starter/script.js
@@ -258,4 +258,17 @@ const secureBooking = function(){
 
   }
 }
-const booker = secureBooking
+const booker = secureBooking;
+
+//139 Coding Challenge #2
+
+
+(function() {
+  const header = document.querySelector('h1');
+  header.style.color = 'blue';
+
+  document.querySelector('body').addEventListener('click', function() {
+    header.style.color = 'red';
+  })
+
+})();
