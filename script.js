7298e8ab03348f91ae467e8c465462abdebe28b1
CARLOS G T ALMEIDA <105361453+workstutoriais@users.noreply.github.com>
2023-10-26 00:46:43 +0200

    15-mapty

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-'use strict';
+"use strict";
 
-const x = '23';
+const x = "23";
 
 if (x == 23) console.log(23);
 
-const calcage = birthYear => 2023 - birthYear;
+const calcage = (birthYear) => 2023 - birthYear;
 
 console.log(x);
 
@@ -14,8 +14,8 @@ We work for a company building a smart home thermoter. Our most recent task
  is this: "Given an array of temperatures of one day, calculate the temperature amplitude.
  Keep in mind that sometimes there might be an sensor error. "
 */
-const temperatures = [3, -2, -6, -1, 'error', 9, 13, 17, 15, 14, 9, 5];
-const temperatures2 = [-12, 'error', 9, 13, 38];
+const temperatures = [3, -2, -6, -1, "error", 9, 13, 17, 15, 14, 9, 5];
+const temperatures2 = [-12, "error", 9, 13, 38];
 
 // 1)understanding the problem
 //What is temp amplitude? Answer: difference between highst and lowest temp
@@ -33,7 +33,7 @@ const calcTempAmplitude = function (temps = []) {
   let min = temps[0];
 
   for (let i = 0; i < temps.length; i++) {
-    if (typeof temps[i] !== 'number') {
+    if (typeof temps[i] !== "number") {
       console.log(temps[i]);
       continue;
     } else if (temps[i] > max) {
@@ -58,7 +58,7 @@ const calcTempAmplitudeNew = function (temps = [], temps2 = []) {
   let min = mergedArrays[0];
 
   for (let i = 0; i < mergedArrays.length; i++) {
-    if (typeof mergedArrays[i] !== 'number') {
+    if (typeof mergedArrays[i] !== "number") {
       console.log(mergedArrays[i]);
       continue;
     } else if (mergedArrays[i] > max) {
